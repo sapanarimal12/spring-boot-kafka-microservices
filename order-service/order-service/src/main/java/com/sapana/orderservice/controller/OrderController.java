d3febd6fd5a0d8fb5e85a2190f8ae65263416613
sapanarimal12 <rimalsapanaaa55@gmail.com>
2024-11-06 13:21:13 +0545

    added dummy api

--- order-service/order-service/src/main/java/com/sapana/orderservice/controller/OrderController.java
+++ order-service/order-service/src/main/java/com/sapana/orderservice/controller/OrderController.java
@@ -4,11 +4,11 @@ import com.sapana.basedomains.dto.Order;
 import com.sapana.basedomains.dto.OrderEvent;
 import com.sapana.orderservice.kafka.OrderProducer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -33,5 +33,26 @@ public class OrderController {
 
         orderProducer.sendMessage(orderEvent);
         return "Order placed Successfully";
+    }
+
+    @GetMapping("/{id}")
+    public Map<String, String> getOrderById(@PathVariable String id) {
+        return Map.of(
+                "id", id,
+                "name", "DEll",
+                "price", "50000"
+        );
+    }
+    @GetMapping()
+    public List<Order> getOrderList() {
+        List<Order> orders = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Order order = new Order();
+            order.setOrderId(UUID.randomUUID().toString());
+            order.setName("DELL " + (i + 1));
+            order.setPrice(50000 + (i * 1000)); // Increment price for variety
+            orders.add(order);
+        }
+        return orders;
     }
 }
